df8bdc12c594415e1ddf36a979ea658e1333bece
leverage2014 <myletter428@gmail.com>
2016-08-13 19:13:28 -0400

    Add services

--- www/js/app.js
+++ www/js/app.js
@@ -33,42 +33,40 @@
     //   title: 'Third Note',
     //   description: 'This is my third note.'
     // }];
-    return [];
+    var notes = [];
+    return {
+      list: function() {
+        return notes;
+      },
+      getNote: function(noteId) {
+        for (var i = 0; i < notes.length; i++) {
+          if (notes[i].id == noteId) {
+            return angular.copy(notes[i]);
+          }
+        }
+        return undefined;
+      },
+      updateNote: function(noteId, note) {
+        for (var i = 0; i < notes.length; i++) {
+          if (notes[i].id == noteId) {
+            notes[i] = angular.copy(note);
+          }
+        }
+      }
+    };
   });
 
   app.controller('ListCtrl', ['$scope', 'notes', function($scope, notes) {
-    $scope.notes = notes;
+    $scope.notes = notes.list();
   }]);
 
   app.controller('EditCtrl', ['$scope', '$state', 'notes', function($scope, $state, notes) {
     $scope.noteId = $state.params.noteId;
-    //  $scope.notes = notes;
-    $scope.notes = angular.copy(notes);
-
-    $scope.getNote = function(noteId) {
-      for (var i = 0; i < $scope.notes.length; i++) {
-        if ($scope.notes[i].id == noteId) {
-          return $scope.notes[i];
-        }
-      }
-      return undefined;
-    }
-
-    $scope.selectNote = $scope.getNote($scope.noteId);
-
-    $scope.updateNote = function(noteId, note) {
-      for (var i = 0; i < notes.length; i++) {
-        if (notes[i].id == noteId) {
-          notes[i] = angular.copy(note);
-        }
-      }
-    }
-
+    $scope.selectNote = notes.getNote($scope.noteId);
     $scope.save = function() {
-      $scope.updateNote($scope.noteId, $scope.selectNote);
+      notes.updateNote($scope.noteId, $scope.selectNote);
       $state.go('list');
     }
-
   }]);
 
   app.controller('AddCtrl', ['$scope', '$state', 'notes', function($scope, $state, notes) {
@@ -76,7 +74,7 @@
     $scope.add = function() {
       $scope.note.id = new Date().getTime().toString();
       console.log('new note', $scope.note);
-      notes.push(angular.copy($scope.note));
+      notes.list().push(angular.copy($scope.note));
       $state.go('list');
     }
   }]);

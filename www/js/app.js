9533e2191bc530f4db980a8c12e9e952b2d7804e
leverage2014 <myletter428@gmail.com>
2016-08-13 18:29:52 -0400

    Add edit functionality

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,6 @@
 // Ionic Starter App
 (function() {
   var app = angular.module('myNotes', ['ionic']);
-  app.controller('ListCtrl', ['$scope', function($scope) {
-    $scope.notes = [{
-      title: 'First Note',
-      description: 'This is my first note.'
-    }, {
-      title: 'Second Note',
-      description: 'This is my second note.'
-    }, {
-      title: 'Third Note',
-      description: 'This is my third note.'
-    }];
-  }]);
 
   app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/list');
@@ -22,10 +10,61 @@
         templateUrl: 'templates/list.html'
       })
       .state('edit', {
-        url: '/edit',
+        url: '/edit/:noteId',
         templateUrl: 'templates/edit.html'
       });
   });
+
+  app.service('notes', function() {
+    return [{
+      id: '1',
+      title: 'First Note',
+      description: 'This is my first note.'
+    }, {
+      id: '2',
+      title: 'Second Note',
+      description: 'This is my second note.'
+    }, {
+      id: '3',
+      title: 'Third Note',
+      description: 'This is my third note.'
+    }];
+  });
+
+  app.controller('ListCtrl', ['$scope', 'notes', function($scope, notes) {
+    $scope.notes = notes;
+  }]);
+
+  app.controller('EditCtrl', ['$scope', '$state', 'notes', function($scope, $state, notes) {
+    $scope.noteId = $state.params.noteId;
+    //  $scope.notes = notes;
+    $scope.notes = angular.copy(notes);
+
+    $scope.getNote = function(noteId) {
+      for (var i = 0; i < $scope.notes.length; i++) {
+        if ($scope.notes[i].id == noteId) {
+          return $scope.notes[i];
+        }
+      }
+      return undefined;
+    }
+
+    $scope.selectNote = $scope.getNote($scope.noteId);
+
+    $scope.updateNote = function(noteId, note) {
+      for (var i = 0; i < notes.length; i++) {
+        if (notes[i].id == noteId) {
+          notes[i] = angular.copy(note);
+        }
+      }
+    }
+
+    $scope.save = function() {
+      $scope.updateNote($scope.noteId, $scope.selectNote);
+      $state.go('list');
+    }
+
+  }]);
 
   app.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
